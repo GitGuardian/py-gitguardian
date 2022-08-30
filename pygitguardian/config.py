fccb195d00298a15e9c14b41147018f646f57b0c
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2022-08-30 10:14:48 +0200

    chore: increase upload limit
    And make it configurable with an environment variable.

--- pygitguardian/config.py
+++ pygitguardian/config.py
@@ -1,6 +1,13 @@
+import os
+
+
 DEFAULT_BASE_URI = "https://api.gitguardian.com"
 DEFAULT_API_VERSION = "v1"
 DEFAULT_TIMEOUT = 20.0  # 20s default timeout
 
 MULTI_DOCUMENT_LIMIT = 20
-DOCUMENT_SIZE_THRESHOLD_BYTES = 1048576  # 1MB
+
+try:
+    DOCUMENT_SIZE_THRESHOLD_BYTES = int(os.environ["GG_MAX_FILE_SIZE"])
+except KeyError:
+    DOCUMENT_SIZE_THRESHOLD_BYTES = 10 * 1024 * 1024
