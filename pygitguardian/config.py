183705317546937fde9bddadda4dddfdb6f23116
Garance Gourdel <garance.gourdel@gmail.com>
2024-07-01 17:57:19 +0200

    feat(metadata): add remediation messages

--- pygitguardian/config.py
+++ pygitguardian/config.py
@@ -5,3 +5,16 @@ DEFAULT_TIMEOUT = 20.0  # 20s default timeout
 MULTI_DOCUMENT_LIMIT = 20
 DOCUMENT_SIZE_THRESHOLD_BYTES = 1048576  # 1MB
 MAXIMUM_PAYLOAD_SIZE = 2621440  # 25MB
+
+DEFAULT_PRE_COMMIT_MESSAGE = """Since the secret was detected before the commit was made:
+1. replace the secret with its reference (e.g. environment variable).
+2. commit again."""
+
+DEFAULT_PRE_PUSH_MESSAGE = """Since the secret was detected before the push BUT after the commit, you need to:
+1. rewrite the git history making sure to replace the secret with its reference (e.g. environment variable).
+2. push again."""
+
+DEFAULT_PRE_RECEIVE_MESSAGE = """A pre-receive hook set server side prevented you from pushing secrets.
+Since the secret was detected during the push BUT after the commit, you need to:
+1. rewrite the git history making sure to replace the secret with its reference (e.g. environment variable).
+2. push again."""
